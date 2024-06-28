f7c2ba4d7043a0e7b610237406f2afc57196bdd6
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 11:12:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import requests
 from st_aggrid import AgGrid
 
 #baca dataframe dari file csv 
-titanic = pd.read_csv('house_clean.csv')
-json()
+house = pd.read_csv('house_clean.csv')
 
 def main() : 
   st.write('Minimal Example')
@@ -25,13 +24,10 @@ if __name__ == '__main__' :
 
 def main() : 
   st.write('Contoh dataframe')
-  st.dataframe(titanic)
+  st.dataframe(house)
   st.write('Contoh JSON')
-  st.json(flight_passanger_api)
-  st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
   st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(titanic)
+  AgGrid(house)
   st.table([x for x in range(1,5)])
 if __name__ == '__main__' : 
   main()
