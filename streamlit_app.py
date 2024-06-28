41f8a5ea348a1bff9335b711584947887d7cc483
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 10:01:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ def main() :
 
 if __name__ == '__main__' : 
   main()
+
+def main() : 
+  st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
+
+if __name__ == '__main__' : 
+  main()
