20a4428c80b4062d007c85e43f25ca3ca1801cd0
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 11:21:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st 
 import pandas as pd 
-import requests
-from st_aggrid import AgGrid
 
 #baca dataframe dari file csv 
 house = pd.read_csv('house_clean.csv')
@@ -19,7 +17,9 @@ def main() :
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
 
+  st.write('Contoh dataframe')
   st.dataframe(house)
+  
   st.write('Metrics')
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
   
