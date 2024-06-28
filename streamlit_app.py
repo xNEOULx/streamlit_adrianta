6c7ea50692adf3c3c10d50d1326c3730f4ad97ea
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 14:12:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,12 +96,6 @@ tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
 tab1.write("this is tab 1")
 tab2.write("this is tab 2")
 
-# You can also use "with" notation:
-with tab1:
-    st.radio("Select one:", [1, 2])
-with tab2:
-    st.radio("Select one:", [1, 2])
-
 
 if __name__ == '__main__' : 
   main()
