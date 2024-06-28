686476d779d8c7a7b8228e8297365063c5ae71d6
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 09:54:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,7 @@
 import streamlit as st 
 
 def main() : 
-  st.header('This is Header')
-  st.subheader('This is SubHeader')
-  st.markdown('# Rendering Markdown ')
-  st.write('Some Phytagorean Equation : ')
-  st.latex('c^2 = a^2+b^2')
+  st.write('Minimal Example')
 
 if __name__ == '__main__' : 
   main()
