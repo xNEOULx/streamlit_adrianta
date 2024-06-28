65dd8bfbe3ac5e8d80ff0f1aefb96208d023c719
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 11:16:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,7 @@ import requests
 from st_aggrid import AgGrid
 
 #baca dataframe dari file csv 
-titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
-#read json file dari data covid 
-flight_passanger_api = requests.post('https://forecastpassengerapi.herokuapp.com/forecast_timeseries',json={
-  "month_limit": "2020-01-01",
-  "window_size": 12
-}).json()
+house = pd.read_csv('house_clean.csv')
 
 def main() : 
   st.write('Minimal Example')
@@ -27,15 +22,6 @@ def main() :
 if __name__ == '__main__' : 
   main()
 
-def main() : 
-  st.write('Contoh dataframe')
-  st.dataframe(titanic)
-  st.write('Contoh JSON')
-  st.json(flight_passanger_api)
-  st.write('Metrics')
+
+  st.dataframe(house)
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-  st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(titanic)
-  st.table([x for x in range(1,5)])
-if __name__ == '__main__' : 
-  main()
