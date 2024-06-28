be9d0f210622ae4630acea5222c57da16ddbfead
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 13:42:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,14 @@ def main() :
   radio_button= st.radio('Choose below',[x for x in range(1,3)])
   st.write('Anda Memilih',radio_button)
 
+# menampilkan slider
+  age_slider = st.slider('Berapa Usia Anda',0,100)
+  st.write('Usia Anda',age_slider)
+
+# menampilkan input typing
+
+  num_input = st.number_input('Input Berapapun')
+  st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
 
 if __name__ == '__main__' : 
   main()
