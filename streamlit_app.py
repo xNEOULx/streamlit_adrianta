36ca0e0931c34f64c24e5fc7bbd364aeae9add51
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 13:25:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd 
+from st_aggrid import AgGrid
 
 #baca dataframe dari file csv 
 house = pd.read_csv('house_clean.csv')
@@ -26,6 +27,9 @@ def main() :
   st.write('Metrics')
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
-  
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(titanic)
+  st.table([x for x in range(1,5)])
+
 if __name__ == '__main__' : 
   main()
