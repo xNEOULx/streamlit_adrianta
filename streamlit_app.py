ae2481b719df00f5b22df0cc86d8c5deac2b1f85
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 13:37:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,15 @@ def main() :
   st.write('Metrics')
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
-  st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(house)
-  st.table([x for x in range(1,5)])
+  click_me_btn = st.button('Click Me')
+  st.write(click_me_btn) #Return True kalo di Click 
+  check_btn = st.checkbox('Klik Jika Setuju')
+  if check_btn :
+      st.write('Anda Setuju')
+    
+    
+  radio_button= st.radio('Choose below',[x for x in range(1,3)])
+   st.write('Anda Memilih',radio_button)
 
 
 if __name__ == '__main__' : 
