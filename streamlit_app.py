51fdafd4e7f3608f0205c6da6144a36e88b31363
Adrianta Ras Sembiring <140039777+xNEOULx@users.noreply.github.com>
2024-06-28 14:10:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,5 +91,15 @@ def main() :
 
   st.write("Outside the form")
 
+# Insert containers separated into tabs:
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1.write("this is tab 1")
+tab2.write("this is tab 2")
+
+# You can also use "with" notation:
+with tab1:
+    st.radio("Select one:", [1, 2])
+
+
 if __name__ == '__main__' : 
   main()
